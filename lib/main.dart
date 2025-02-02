3fabf1c4e8bbef0348970a5916a9b5508883a3a0
Prateek-Prakash <63609044+Prateek-Prakash@users.noreply.github.com>
2025-02-02 12:02:59 -0500

    Add Theming Class

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:stix/go_router_config.dart';
+import 'package:stix/theming.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,12 +23,9 @@ class Application extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Stix',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
-        ),
-        useMaterial3: true,
-      ),
+      themeMode: ThemeMode.system,
+      theme: lightThemeData,
+      darkTheme: darkThemeData,
       routerConfig: GoRouterConfig.goRouter,
     );
   }
---
+++ lib/theming.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final visualDensity = VisualDensity.compact;
+final fontFamily = GoogleFonts.ubuntu().fontFamily;
+final fontFamilyMono = GoogleFonts.ubuntuMono().fontFamily;
+
+final lightThemeData = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.deepPurple,
+    brightness: Brightness.light,
+  ),
+  fontFamily: fontFamily,
+  useMaterial3: true,
+);
+
+final darkThemeData = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.deepPurple,
+    brightness: Brightness.light,
+  ),
+  fontFamily: fontFamily,
+  useMaterial3: true,
+);
